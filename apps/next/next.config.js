f238b4e82c77c5a58ffe6002c0e22f5f4ab8a2eb
Diego Melendez <diedu89@gmail.com>
2022-08-09 22:05:01 -0600

    apply fix

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -1,32 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 const { withNativebase } = require('@native-base/next-adapter')
+const withFonts = require('next-fonts')
+const { withExpo } = require('@expo/next-adapter')
 
 module.exports = withNativebase({
-  dependencies: [
-    '@expo/next-adapter',
-    'react-native-vector-icons',
-    'react-native-vector-icons-for-web',
-    'solito',
-    'app',
-  ],
+  dependencies: ['@expo/next-adapter', 'solito', 'app'],
+  plugins: [withFonts, [withExpo, { projectRoot: __dirname }]],
   nextConfig: {
     projectRoot: __dirname,
     reactStrictMode: true,
     webpack5: true,
-    webpack: (config, options) => {
-      config.resolve.alias = {
-        ...(config.resolve.alias || {}),
-        'react-native$': 'react-native-web',
-        '@expo/vector-icons': 'react-native-vector-icons',
-      }
-      config.resolve.extensions = [
-        '.web.js',
-        '.web.ts',
-        '.web.tsx',
-        ...config.resolve.extensions,
-      ]
-      return config
-    },
   },
 })
